5086ca82b7bfbe8ec6e78c78a05d1999f17f0cb8
Thiago Vilella Waideman Puga <thiago_puga@outlook.com>
2015-06-08 13:30:57 -0700

    Update

---
+++ GPS/ReadingPPS.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+
+PPS_PIN = 12
+
+# Setup GPIO using Board numbering
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(PPS_PIN, GPIO.IN)
+
+while True:
+    
+    if GPIO.input(PPS_PIN):
+        print(1)
+    else:
+        print(0)
