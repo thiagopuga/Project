7bd44bbfa6db505b5d62ae5ba887997ad59dbc93
Thiago Vilella Waideman Puga <thiago_puga@outlook.com>
2015-06-30 17:35:10 -0700

    Update

--- SmartGridServer.py
+++ SmartGridServer.py
@@ -11,19 +11,26 @@ sock.bind(server_address)
 
 fileName = ""
 
-while True:
-    print >>sys.stderr, "waiting to receive message"
-    data, address = sock.recvfrom(4096)
-    print >>sys.stderr, "received %s bytes from %s" % (len(data), address)
-    # Split data
-    resp = data.split(',')
-    date = resp[0]
-    string = resp[1]    
-    # Create a log file    
-    if date != fileName:
-        if fileName != "":
-            file.close()                           
-        fileName = date
-        file = open(fileName + ".log", 'a')
-    # Write on log
-    file.write(string + "\n")
+try:
+    while True:
+        print >>sys.stderr, "waiting to receive message"
+        data, address = sock.recvfrom(4096)
+        print >>sys.stderr, "received %s bytes from %s" % (len(data), address)
+        # Split data
+        resp = data.split(',')
+        date = resp[0]
+        string = resp[1]    
+        # Create a log file    
+        if date != fileName:
+            if fileName != "":
+                file.close()                           
+            fileName = date
+            file = open(fileName + ".log", 'a')
+        # Write on log
+        file.write(string + "\n")
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
+    print >>sys.stderr, 'closing log'
+    file.close()
