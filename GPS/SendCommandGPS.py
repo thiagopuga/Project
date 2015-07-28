d3c45e920dc984e2380c99a104509ddfc773ca34
Thiago Vilella Waideman Puga <thiago_puga@outlook.com>
2015-07-28 15:48:27 -0700

    Update

---
+++ GPS/SendCommandGPS.py
@@ -0,0 +1,31 @@
+import serial
+import sys
+
+try:
+    serial = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=3)
+    
+except:
+    print 'error opening serial port'
+
+resp = ''
+
+try:
+    # PMTK_SET_NMEA_UPDATE_10HZ
+    serial.write('$PMTK220,100*2F\r\n')
+    # PMTK_SET_NMEA_OUTPUT_RMCONLY
+    serial.write('$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n')
+    
+    print 'OK'
+    
+    while True:
+        while (serial.inWaiting() > 0):
+            resp += serial.read()            
+            if '\r\n' in resp:
+                print resp
+                resp = ''
+
+except:
+    print sys.exc_info()
+
+finally:
+    serial.close()
